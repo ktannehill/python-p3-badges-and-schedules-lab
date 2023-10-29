206bd70545e8850c18ff7d18c1154dc580f6008a
Kat Tannehill <katetannehill@gmail.com>
2023-10-28 21:18:04 -0600

    Complete lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,24 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(f"Hello, my name is {name}.")
+    return badges
 
 def assign_rooms(names):
-    return None
+    rooms = []
+    index = 0
+    # while index < len(names):
+    # for index, name in enumerate(names):
+    for index in range(len(names)):
+        rooms.append(f"Hello, {names[index]}! You'll be assigned to room {index + 1}!")
+        # index += 1
+    return rooms
 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+        print(badge)
+    for thing in assign_rooms(names):
+        print(thing)
